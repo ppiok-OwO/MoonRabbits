42f5f7925ae056aaeca104cdad5957361fe5839a
ppiok-OwO <solcorin@naver.com>
2025-02-05 00:24:17 +0900

    fix: 임포트 문제 수정

--- src/handlers/town/townEnter.handler.js
+++ src/handlers/town/townEnter.handler.js
@@ -1,49 +1,53 @@
-import { config } from "../../config/config.js";
+import { config } from '../../config/config.js';
 import makePacket from '../../utils/packet/makePacket.js';
 import payload from '../../utils/packet/payload.js';
+import payloadData from '../../utils/packet/payloadData.js';
 
-//import payloadData from '../utils/packet/payloadData.js'
 const townEnterHandler = (socket, packetData) => {
-    let response = {
-        player: {
-            playerId: 123,
-            nickname: packetData.nickname,
-            class: packetData.class,
-            transform: { posX: 1, poxY: 1, posZ: 1, rot: 1 },
-            statInfo: {
-                level: 1,
-                hp: 10,
-                maxHp: 10,
-                mp: 10,
-                maxMp: 10,
-                atk: 1,
-                def: 1,
-                magic: 1,
-                speed: 1,
-            },
-        },
-    };
+  let response = {
+    player: {
+      playerId: 123,
+      nickname: packetData.nickname,
+      class: packetData.class,
+      transform: { posX: 1, poxY: 1, posZ: 1, rot: 1 },
+      statInfo: {
+        level: 1,
+        hp: 10,
+        maxHp: 10,
+        mp: 10,
+        maxMp: 10,
+        atk: 1,
+        def: 1,
+        magic: 1,
+        speed: 1,
+      },
+    },
+  };
 
+  // 처단 대상
+  console.log(packetData.nickname);
+  console.log(packetData.class);
 
-    // 처단 대상
-    console.log(packetData.nickname);
-    console.log(packetData.class);
+  const transform = payloadData.TransformInfo(1, 1, 1, 1);
+  console.log(transform);
+  const statInfo = payloadData.StatInfo(1, 10, 10, 10, 10, 10, 10, 10, 10);
+  console.log(statInfo);
+  const playerInfo = payloadData.PlayerInfo(
+    123,
+    packetData.nickname,
+    packetData.class,
+    transform,
+    statInfo,
+  );
+  console.log(playerInfo);
+  const data = payload.S_Enter(playerInfo);
+  console.log(data);
 
-    const transform = payloadData.TransformInfo(1, 1, 1, 1);
-    console.log(transform);
-    const statInfo = payloadData.StatInfo(1, 10, 10, 10, 10, 10, 10, 10, 10);
-    console.log(statInfo);
-    const playerInfo = payloadData.PlayerInfo(123, packetData.nickname, packetData.class, transform, statInfo);
-    console.log(playerInfo);
-    const data = payload.S_Enter(playerInfo);
-    console.log(data);
+  const temp = makePacket(config.packetId.S_Enter, data);
+  console.log(temp);
 
-    const temp = makePacket(config.packetId.S_Enter, data);
-    console.log(temp);
-
-    socket.write(makePacket(config.packetId.S_Enter, data));
-    console.log('응답 후 로그');
-}
-
+  socket.write(makePacket(config.packetId.S_Enter, data));
+  console.log('응답 후 로그');
+};
 
 export default townEnterHandler;
