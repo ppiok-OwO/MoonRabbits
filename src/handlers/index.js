80ac061d2ab03fd8c1cc75436eb1fd4dab975782
dwook <dwook1637@gmail.com>
2025-02-04 19:48:10 +0900

    [feat] 테스트 핸들러 구현

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,13 @@
+import { config } from "../config/config.js";
+import makePacket from "../utils/packet/makePacket.js";
+
 // 패킷 ID별로 핸들러 맵핑
 const handlers = {
-    // 0: C_Enter,
+    0: (socket, packetData) => {
+        let response = { playerId:123, nickname:packetData.nickname, class:444, transform:{posX:0, poxY:0, posZ:0, rot:0}, statInfo:{level:1, hp:10, maxHp:10, mp:10, maxMp:10, atk:1, def:1, magic:1, speed:1}};
+        socket.write(makePacket(config.packetId.S_Enter, response));
+        console.log('응답 후 로그');
+    },
     // 6: C_Move,
     // 8: C_Animation ,
     // 12: C_Chat,
