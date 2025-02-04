0ef8b0cc3d28be219629259528173cd7bb57f155
dwook <dwook1637@gmail.com>
2025-02-04 19:54:03 +0900

    [feat] import 추가

--- src/utils/packet/makePacket.js
+++ src/utils/packet/makePacket.js
@@ -1,6 +1,17 @@
-import printPacket from "../log/printPacket";
+import { config } from "../../config/config.js";
+import { getProtoMessages } from "../../init/loadProtos.js";
+import printPacket from "../log/printPacket.js";
 
-function makePacket(packetId, payload) {
+function makePacket(packetId, payload) {   
+    // 패킷 아이디 -> 타입
+    const packetIdValues = Object.values(config.packetId);
+    const packetIdIndex = packetIdValues.findIndex((f) => f === packetId,);
+    const packetType = Object.keys(config.packetId)[packetIdIndex];
+
+    // 페이로드
+    const proto = getProtoMessages()[packetType];
+    const packetDataBuffer = proto.encode(payload).finish();
+
     // 패킷 크기
     const packetSize = 5 + packetDataBuffer.length;
 
@@ -17,23 +28,14 @@ function makePacket(packetId, payload) {
       packetSizeBuffer,
       packetIdBuffer,
     ]);
-    
-    // 패킷 아이디 -> 타입
-    const packetIdValues = Object.values(config.packetId);
-    const packetIdIndex = packetIdValues.findIndex((f) => f === packetId,);
-    const packetType = Object.keys(config.packetId)[packetIdIndex];
-
-    // 페이로드
-    const proto = getProtoMessages()[packetType];
-    const packetDataBuffer = proto.encode(payload).finish();
-
-    // 패킷 만들기
-    const packetBuffer = Buffer.concat([headerBuffer, packetDataBuffer]);
-
+ 
     // 디버그용 콘솔 출력, packetId 필터링해서 사용
     if (packetId >= 0) {
       printPacket(packetSize, packetId, payload, 'out');
     }
+
+    // 패킷 만들기
+    const packetBuffer = Buffer.concat([headerBuffer, packetDataBuffer]);
 
     return packetBuffer;
 }
