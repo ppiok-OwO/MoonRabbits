d0ec695cc2a6f464c3b12c4c71e07a723d2f2898
dwook <dwook1637@gmail.com>
2025-02-04 19:27:15 +0900

    [feat] 패킷 관련 코드 추가

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,1 +1,12 @@
 // 패킷 ID별로 핸들러 맵핑
+const handlers = {
+    // 0: C_Enter,
+    // 6: C_Move,
+    // 8: C_Animation ,
+    // 12: C_Chat,
+    // 14: C_EnterDungeon,
+    // 15: C_PlayerResponse,
+}
+
+export default handlers;
+  
