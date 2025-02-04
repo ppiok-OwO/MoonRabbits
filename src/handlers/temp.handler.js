0c802dab23382d3a5a7d72dd17ecea670f709fc9
songwongbin <bb201802@naver.com>
2025-02-04 20:17:45 +0900

    hotfix : 미사용 파일 제거

--- src/handlers/temp.handler.js
+++
@@ -1,8 +0,0 @@
-import { socketManager } from '../session/sessions.js';
-
-const tempHandler = (socket, payload) => {
-  // 이런 저런 처리
-
-  socketManager.sendPacket(playerId);
-  socketManager.sendNotification();
-};
--- src/session/sessions.js
+++ src/session/sessions.js
@@ -1,13 +1,5 @@
-import SocketManager from '../classes/manager/socket.manager.js';
+import UserSession from '../classes/session/userSession.class.js';
+import DungeonSession from '../classes/session/dungeonSession.class.js';
 
-export const gameSessions = new Map();
-
-export const socketManager = new SocketManager();
-
-
-
-// 1. 사용자 한 명 한테만 보내기
-// 2. 접속 중인 모든 사용자에게 보내기
-// 3. 특정 파티에게만 보내기
-// 4. 던전 입장 중인 사용자에게 보내기
-// 5. 
+export const userSession = new UserSession();
+export const dungeonSession = new DungeonSession();
