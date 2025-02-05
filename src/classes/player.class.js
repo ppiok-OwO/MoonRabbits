9050a6365bf99d42922abf79e71c8534b6787ffb
foreiner <74106693+foreiner@users.noreply.github.com>
2025-02-05 14:12:47 +0900

    feat: stats class 추가 및 관련 수정
    이후 몬스터/캐릭터 일괄 관리 용

--- src/classes/player.class.js
+++ src/classes/player.class.js
@@ -1,21 +1,34 @@
 import TransformInfo from './transformInfo.class.js';
 import User from './user.class.js';
-
+import stats from './stats.class.js';
+import payloadData from '../utils/packet/payloadData.js';
 class Player extends User {
   constructor(socket, playerId) {
     super(socket);
     this.id = playerId;
     this.position = new TransformInfo();
-    this.level = null;
     this.stat = null;
     this.class = null;
     this.dungeonId = null;
   }
 
-  setPlayerInfo(level, stat, classCode) {
-    this.level = level;
+  setPlayerInfo(stat = new stats(), classCode) {
     this.stat = stat;
     this.class = classCode;
+  }
+  getPlayerStatus() {
+    return payloadData.PlayerStatus(
+      this.class,
+      this.stat.getLevel(),
+      this.nickname,
+      this.stat.getMaxHp(),
+      this.stat.getMaxMp(),
+      this.stat.getHp(),
+      this.stat.getMp(),
+    );
+  }
+  getStatInfo(){
+    return this.stat.getPlayerStats();
   }
 
   setDungeonId(dungeonId) {
