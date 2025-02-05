3d1ed96e354bcdb52dd9cb121968743a01eba93c
wjdxo2412 <wjdxo2412@gmail.com>
2025-02-05 14:47:12 +0900

    fix : 플레이어 참조 코드 변경 테스트 완료

--- src/handlers/town/townEnter.handler.js
+++ src/handlers/town/townEnter.handler.js
@@ -1,8 +1,5 @@
 import { config } from '../../config/config.js';
-import {
-  getDungeonSessions,
-  getPlayerSession,
-} from '../../session/sessions.js';
+import { getPlayerSession } from '../../session/sessions.js';
 import makePacket from '../../utils/packet/makePacket.js';
 import payload from '../../utils/packet/payload.js';
 import payloadData from '../../utils/packet/payloadData.js';
@@ -10,7 +7,7 @@ import payloadData from '../../utils/packet/payloadData.js';
 const townEnterHandler = (socket, packetData) => {
   const transform = payloadData.TransformInfo(1, 1, 1, 1);
   const statInfo = payloadData.StatInfo(1, 10, 10, 10, 10, 10, 10, 10, 10);
-  const player = playerSession.getPlayer(socket);
+  const player = getPlayerSession().getPlayer(socket);
   const playerInfo = payloadData.PlayerInfo(
     player.id,
     packetData.nickname,
@@ -22,8 +19,6 @@ const townEnterHandler = (socket, packetData) => {
   const data = payload.S_Enter(playerInfo);
 
   const packet = makePacket(config.packetId.S_Enter, data);
-
-  const playerSession = getPlayerSession();
 
   socket.write(packet);
 };
