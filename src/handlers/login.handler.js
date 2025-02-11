27c4d0fd4cd8899f23ef61f60c5622f2859c5f58
songwongbin <bb201802@naver.com>
2025-02-11 16:10:17 +0900

    hotfix : 로그인 핸들러 오류 수정
    1. 전개구문 문법 실수

--- src/handlers/login.handler.js
+++ src/handlers/login.handler.js
@@ -3,10 +3,11 @@ import payloadData from '../utils/packet/payloadData.js';
 
 /* (임시) 로그인 패킷 테스트 */
 const loginHandler = (socket, packetData) => {
+  
   // [case 01] 로그인 성공 - 캐릭터 보유
   const isSuccess = true;
   const msg = '로그인에 성공했습니다. 캐릭터 O';
-  const ownedCharacters = [...payloadData.OwnedCharacters('test', 1001)];
+  const ownedCharacters = [payloadData.OwnedCharacters('test', 1001)];
 
   // [case 02] 로그인 성공 - 캐릭터 미비
   // const isSuccess = true;
--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -4,8 +4,8 @@ import Packet from '../utils/packet/packet.js';
 const registerHandler = (socket, packetData) => {
   // [case 01] 회원가입 성공
   const isSuccess = true;
-
   const msg = '회원가입에 성공했습니다.';
+  
   // [case 02] 회원가입 실패
   // const isSuccess = false;
   // const msg = '회원가입에 실패했습니다.';
