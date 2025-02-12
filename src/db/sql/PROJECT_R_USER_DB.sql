2e5f3824c8f9e1ae5bfd12c70641df61838e7b4d
wakelight23 <rlghks0223@gmail.com>
2025-02-12 20:56:46 +0900

    Update : sql, login, register 업데이트
    - sql migrate 정의
    - 회원가입이 가능합니다.
    - 로그인은 아직 미완성입니다.

--- src/db/sql/PROJECT_R_USER_DB.sql
+++ src/db/sql/PROJECT_R_USER_DB.sql
@@ -1,6 +1,6 @@
 -- Active: 1733106386089@@218.237.144.112@3306@PROJECT_R_USER_DB
 -- 사용자 테이블
-CREATE TABLE IF NOT EXISTS USERS
+CREATE TABLE IF NOT EXISTS Users
 (
     user_id    VARCHAR(36)  PRIMARY KEY,
     email      VARCHAR(255) UNIQUE NOT NULL,
@@ -9,13 +9,15 @@ CREATE TABLE IF NOT EXISTS USERS
     created_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS PLAYERS
+CREATE TABLE IF NOT EXISTS Players
 (
     player_id  VARCHAR(36)  PRIMARY KEY,
     user_id    VARCHAR(36)  NOT NULL,
     nickname   VARCHAR(255) NOT NULL,
+    class_code VARCHAR(36)  NOT NULL,
     created_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES USERS (user_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Stats
@@ -29,18 +31,20 @@ CREATE TABLE IF NOT EXISTS Stats
     def         INT         NOT NULL,
     magic       INT         NOT NULL,
     speed       FLOAT       NOT NULL,
-    FOREIGN KEY (player_id) REFERENCES PLAYERS (player_id)
-)
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS Inventory
 (
     inventory_id VARCHAR(36) PRIMARY KEY,
     player_id    VARCHAR(36) NOT NULL,
     item_id      VARCHAR(36) NOT NULL,
-    stack       INT         NOT NULL,
+    stack        INT         NOT NULL,
     is_equipped  BOOLEAN     DEFAULT FALSE,
     created_at   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (player_id) REFERENCES PLAYERS (player_id)
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS QuestProgress
@@ -50,8 +54,9 @@ CREATE TABLE IF NOT EXISTS QuestProgress
     quest_id          VARCHAR(36) NOT NULL,
     progress          INT         NOT NULL,
     start_date        TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
-    update_date        TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
-    finish_date        TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (player_id) REFERENCES PLAYERS (player_id)
+    update_date       TIMESTAMP   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    finish_date       TIMESTAMP   DEFAULT NULL,
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -2,12 +2,14 @@ import { v4 as uuidv4 } from 'uuid';
 import pools from '../database.js';
 import { SQL_QUERIES } from './user.queries.js';
 import { toCamelCase } from '../../utils/transformCase.js';
+import chalk from 'chalk';
 import bcrypt from 'bcrypt';
 
+// Email 기반으로 사용자 찾기
 export const findUserByEmail = async (email) => {
   const [rows] = await pools.PROJECT_R_USER_DB.query(SQL_QUERIES.FIND_USER_BY_EMAIL, [email]);
   if (rows.length > 0) {
-    console.log('사용자를 찾았습니다 : ', rows[0]); // 여기서 user_id 값을 확인
+    console.log(`${chalk.green('[DB Log]')} 사용자를 찾았습니다 : ${rows[0]}`); // 여기서 user_id 값을 확인
     const user = toCamelCase(rows[0]);
 
     // updateLastLogin 메서드 추가
@@ -22,6 +24,7 @@ export const findUserByEmail = async (email) => {
   }
 };
 
+// User 생성 시 DB 접근
 export const createUser = async (email, password) => {
   const userId = uuidv4(); // UUID 생성
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -32,6 +35,28 @@ export const createUser = async (email, password) => {
   return { userId, email }; // 새로 생성된 사용자 정보 반환
 };
 
+// user_id 기반으로 플레이어 찾기
+export const findPlayerByUserId = async (userId) => {
+  const [rows] = await pools.PROJECT_R_USER_DB.query(SQL_QUERIES.FIND_PLAYER_BY_USER_ID, [userId]);
+  if (rows.length > 0) {
+    console.log(`${chalk.green('[DB Log]')} 플레이어를 찾았습니다 : ${rows[0]}`); // 여기서 player_id 값을 확인
+    return toCamelCase(rows[0]);
+  } else {
+    return null;
+  }
+};
+
+// 캐릭터 생성 시 DB 접근
+export const createPlayer = async (userId, nickname, classCode) => {
+  const playerId = uuidv4(); // UUID 생성
+
+  // 사용자 생성 쿼리 실행
+  await pools.PROJECT_R_USER_DB.query(SQL_QUERIES.CREATE_PLAYER, [userId, nickname, classCode]);
+
+  return { playerId, userId, nickname, classCode }; // 새로 생성된 사용자 정보 반환
+};
+
+// 사용자 로그인 시, 마지막 로그인 시간 업데이트
 export const updateUserLogin = async (id) => {
   await pools.PROJECT_R_USER_DB.query(SQL_QUERIES.UPDATE_USER_LOGIN, [id]);
 };
