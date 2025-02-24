245ef32fc8600eb6d9a6a78a3e83171fcbced616
foreiner <74106693+foreiner@users.noreply.github.com>
2025-02-24 15:20:49 +0900

    del: 사용 안하는 내용 삭제

--- src/session/sessions.js
+++ src/session/sessions.js
@@ -2,15 +2,11 @@ import PlayerSession from '../classes/session/playerSession.class.js';
 import SectorSession from '../classes/session/sectorSession.class.js';
 import UserSession from '../classes/session/userSession.class.js';
 import PartySession from '../classes/session/partySession.class.js';
-// import MonsterSession from '../classes/session/monsterSession.class.js';
-// import TestSectorSession from '../classes/session/testDungeonSession.class.js';
 
 const playerSession = new PlayerSession();
 const sectorSession = new SectorSession();
 const userSession = new UserSession();
 const partySession = new PartySession();
-// const monsterSession = new MonsterSession();
-// const testDungeonSession = new TestSectorSession();
 
 export const getPlayerSession = () => {
   return playerSession;
@@ -27,11 +23,3 @@ export const getUserSessions = () => {
 export const getPartySessions = () => {
   return partySession;
 };
-
-// export const getMonsterSessions = () => {
-//   return monsterSession;
-// };
-
-// export const getTestDungeonSessions = () => {
-//   return testDungeonSession;
-// };
