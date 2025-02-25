983ed2f2604e581476d7befe196f9a9a41a3d8fb
songwongbin <bb201802@naver.com>
2025-02-25 16:31:30 +0900

    feat: 2섹터 navMesh 파일 추가

--- src/init/navMeshData.js
+++ src/init/navMeshData.js
@@ -5,6 +5,7 @@ export const aSectorNavMesh = await loadNavMesh('Test Exported NavMesh.obj');
 
 // 첫번째 섹터 정식 네브
 export const Sector1NavMesh = await loadNavMesh('Sector1 Exported NavMesh.obj');
+export const Sector2NavMesh = await loadNavMesh('Sector2 Exported NavMesh.obj');
 
 export const getNaveMesh = (sectorCode) => {
   switch (sectorCode) {
