4aee828dd4922db35819642af244ea52a573a8fd
songwongbin <bb201802@naver.com>
2025-02-26 19:30:52 +0900

    feat : 스폰 통신 문제 해결

--- src/handlers/town/playerSpawnNotification.handler.js
+++ src/handlers/town/playerSpawnNotification.handler.js
@@ -2,6 +2,7 @@ import { config } from '../../config/config.js';
 import { getSectorSessions, getPlayerSession } from '../../session/sessions.js';
 import makePacket from '../../utils/packet/makePacket.js';
 import payload from '../../utils/packet/payload.js';
+import { CODE_TO_ID } from '../../utils/tempConverter.js';
 
 // !!! 패킷 정의 변경에 따라 S_Spawn -> S2CPlayerSpawn으로 일괄 수정해씀다
 
@@ -18,11 +19,20 @@ const playerSpawnNotificationHandler = (socket, packetData) => {
   const spawn = payload.S2CSpawn(playerInfoArray);
   const packet = makePacket(config.packetId.S2CSpawn, spawn);
 
-  const sectorId = playerSession.getPlayer(socket).getSectorId();
+  // const sectorId = playerSession.getPlayer(socket).getSectorId();
+  // if (sectorId) {
+  //   const sectorSessions = getSectorSessions();
+  //   const sector = sectorSessions.getSector(sectorId);
+  //   sector?.notify(packet);
+  // } else {
+  //   playerSession.notify(packet);
+  // }
 
-  if (sectorId) {
+  // @@@ getSectorId 메서드가 사실 sectorCode를 가져옴... @@@
+  const sectorCode = playerSession.getPlayer(socket).getSectorId();
+  if (sectorCode) {
     const sectorSessions = getSectorSessions();
-    const sector = sectorSessions.getSector(sectorId);
+    const sector = sectorSessions.getSector(CODE_TO_ID[sectorCode]);
     sector?.notify(packet);
   } else {
     playerSession.notify(packet);
