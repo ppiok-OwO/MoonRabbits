e5598ee2f2f694ba13bbc25864c334144b1aef70
ppiok-OwO <solcorin@naver.com>
2025-02-27 11:24:19 +0900

    feat: 서버 다중화

--- src/server.js
+++ src/server.js
@@ -3,16 +3,24 @@ import initServer from './init/index.js';
 import { onConnection } from './events/onConnection.js';
 import { config } from './config/config.js';
 
-const server = net.createServer(onConnection);
+const PORTS = [3000, 3001, 3002];
 
 initServer()
   .then(() => {
-    server.listen(config.server.port, config.server.host, () => {
-      console.log(`[메인서버]가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
-      console.log(server.address());
+    PORTS.forEach((port) => {
+      const server = net.createServer(onConnection);
+      server.listen(port, config.server.host, () => {
+        console.log(
+          `[메인서버]가 ${config.server.host}:${port}에서 실행 중입니다.`,
+        );
+      });
+
+      server.on('error', (error) => {
+        console.error(`❌ 포트 ${port}에서 서버 실행 중 오류 발생:`, error);
+      });
     });
   })
   .catch((error) => {
     console.error(error);
-    process.exit(1); // 오류 발생 시 프로세스 종료
+    process.exit(1);
   });
