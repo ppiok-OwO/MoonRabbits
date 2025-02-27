5ff091f9fb3fcd36a29e0079c022311196d0ee8d
ppiok-OwO <solcorin@naver.com>
2025-02-27 23:02:27 +0900

    fix: 조건문 이상한 거 수정

--- src/handlers/town/playerLocationUpdate.handler.js
+++ src/handlers/town/playerLocationUpdate.handler.js
@@ -46,10 +46,6 @@ const playerLocationUpdateHandler = (socket, packetData) => {
           closestPoint = { PosX: point.x, PosY: point.y, PosZ: point.z }; // transform과 가장 가까운 경로상의 좌표
         }
       });
-      // @@@ 잠시 주석 처리!!! @@@
-      // console.log('closestPoint : ', closestPoint);
-      // console.log('transform :', transform);
-      // console.log('minDistance :', minDistance);
 
       if (minDistance > 1.4) {
         // 오차범위를 벗어나면 플레이어의 위치를 closestPoint로 재조정한다.
@@ -67,20 +63,6 @@ const playerLocationUpdateHandler = (socket, packetData) => {
           player.getSectorId(),
         );
 
-        // 위치동기화 브로드 캐스트
-        // const sectorId = player.getSectorId();
-        // if (sectorId) {
-        //   // 만약 던전이면
-        //   const sectorSessions = getSectorSessions();
-        //   const sector = sectorSessions.getSector(sectorId);
-        //   sector.notify(packet);
-        //   // dungeon.notify(syncLocationPacket);
-        // } else {
-        //   // 던전이 아니면
-        //   playerSession.notify(packet);
-        // }
-
-        // @@@ getSectorId 메서드가 사실 sectorCode를 가져옴... @@@
         const sectorCode = player.getSectorId();
         if (sectorCode) {
           // 만약 던전이면
@@ -93,38 +75,25 @@ const playerLocationUpdateHandler = (socket, packetData) => {
           playerSession.notify(packet);
         }
         return;
+      } else {
+        const packet = PACKET.S2CPlayerLocation(
+          player.id,
+          transform,
+          true,
+          player.getSectorId(),
+        );
+
+        const sectorCode = player.getSectorId();
+        if (sectorCode) {
+          // 만약 던전이면
+          const sectorSessions = getSectorSessions();
+          const sector = sectorSessions.getSector(CODE_TO_ID[sectorCode]); // 강제로 변환
+          sector.notify(packet);
+        } else {
+          // 던전이 아니면
+          playerSession.notify(packet);
+        }
       }
-    }
-
-    const packet = PACKET.S2CPlayerLocation(
-      player.id,
-      transform,
-      true,
-      player.getSectorId(),
-    );
-
-    // const sectorId = player.getSectorId();
-    // if (sectorId) {
-    //   // 만약 던전이면
-    //   const sectorSessions = getSectorSessions();
-
-    //   const sector = sectorSessions.getSector(sectorId);
-    //   sector.notify(packet);
-    // } else {
-    //   // 던전이 아니면
-    //   playerSession.notify(packet);
-    // }
-
-    // @@@ getSectorId 메서드가 사실 sectorCode를 가져옴... @@@
-    const sectorCode = player.getSectorId();
-    if (sectorCode) {
-      // 만약 던전이면
-      const sectorSessions = getSectorSessions();
-      const sector = sectorSessions.getSector(CODE_TO_ID[sectorCode]); // 강제로 변환
-      sector.notify(packet);
-    } else {
-      // 던전이 아니면
-      playerSession.notify(packet);
     }
   } catch (error) {
     console.error(error);
