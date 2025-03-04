2bb462f2fc0de419a549b81c7a71eebd5617647f
wakelight23 <rlghks0223@gmail.com>
2025-03-04 16:31:40 +0900

    Fix : import 수정

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -34,13 +34,13 @@ import { collisionHandler } from './collision/collision.handler.js';
 import { resourceListHandler } from './gathering/ResourceList.handler.js';
 import setTrapHandler from './playerAction/setTrap.handler.js';
 import removeTrapHandler from './playerAction/removeTrap.handler.js';
-import { pongHandler } from './pong.handler.js';
-import { portalHandler } from './playerAction/portal.handler.js';
 import itemObtainedHandler from './player/inventory/itemObtained.handler.js';
 import itemDisassemblyHandler from './player/inventory/itemDisassembly.handler.js';
 import itemDestroyHandler from './player/inventory/itemDestroy.handler.js';
 import inventorySortHandler from './player/inventory/inventorySort.handler.js';
 import inventoryUpdateHandler from './player/inventory/inventoryUpdate.handler.js';
+import { pongHandler } from './pong.handler.js';
+import { portalHandler } from './playerAction/portal.handler.js';
 
 // !!! 패킷 정의 수정으로 config.packetId 일괄 수정해씀다
 
@@ -84,6 +84,17 @@ const handlers = {
   [config.packetId.C2SRemoveTrap]: removeTrapHandler,
   [config.packetId.C2SStun]: stunHandler,
   [config.packetId.C2SEquipChange]: equipChangeHandler,
+
+  // 인벤토리 관련 핸들러
+  [config.packetId.C2SItemObtained]: itemObtainedHandler,
+  [config.packetId.C2SItemDisassembly]: itemDisassemblyHandler,
+  [config.packetId.C2SItemDestroy]: itemDestroyHandler,
+  [config.packetId.C2SInventorySort]: inventorySortHandler,
+  [config.packetId.C2SItemMove]: inventoryUpdateHandler,
+
+  // 포탈 관련 핸들러
+  [config.packetId.C2SPortal]: portalHandler,
+  [config.packetId.C2SPong]: pongHandler,
 };
 
 export const getHandlerByPacketId = (packetId) => {
