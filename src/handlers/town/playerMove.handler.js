a4d1aa23f0bbad3ef4ad726362a80c8855f8e8bb
ppiok-OwO <solcorin@naver.com>
2025-03-05 15:38:16 +0900

    fix

--- src/handlers/town/playerMove.handler.js
+++ src/handlers/town/playerMove.handler.js
@@ -52,6 +52,7 @@ export async function playerMoveHandler(socket, packetData) {
     // Jmeter용 빈 응답 패킷
     const packet = PACKET.S2CPlayerMove(path);
     socket.write(packet);
+    console.log(packet);
 
     // return isValidPath;
   } catch (error) {
