20499554addfc99adbb41faa65bcb4df3f2bb148
ppiok-OwO <solcorin@naver.com>
2025-03-06 12:23:40 +0900

    test: obj 파일 로드 안 할 때

--- src/init/navMeshData.js
+++ src/init/navMeshData.js
@@ -1,12 +1,17 @@
 import { loadNavMesh } from './navMeshLoader.js';
 
-export const townNavMesh = await loadNavMesh('Town Exported NavMesh.obj');
-export const aSectorNavMesh = await loadNavMesh('Test Exported NavMesh.obj');
+// export const townNavMesh = await loadNavMesh('Town Exported NavMesh.obj');
+export const townNavMesh = null;
+// export const aSectorNavMesh = await loadNavMesh('Test Exported NavMesh.obj');
+export const aSectorNavMesh = null;
 
 // 첫번째 섹터 정식 네브
-export const Sector1NavMesh = await loadNavMesh('Sector1 Exported NavMesh.obj');
-export const Sector2NavMesh = await loadNavMesh('Sector2 Exported NavMesh.obj');
-export const Sector3NavMesh = await loadNavMesh('Sector3 Exported NavMesh.obj');
+// export const Sector1NavMesh = await loadNavMesh('Sector1 Exported NavMesh.obj');
+export const Sector1NavMesh = null;
+// export const Sector2NavMesh = await loadNavMesh('Sector2 Exported NavMesh.obj');
+export const Sector2NavMesh = null;
+// export const Sector3NavMesh = await loadNavMesh('Sector3 Exported NavMesh.obj');
+export const Sector3NavMesh = null;
 
 export const getNavMesh = (sectorCode) => {
   switch (sectorCode) {
