510cf8453bd9cb2e0ada0274e4101fc1fd948a60
foreiner <74106693+foreiner@users.noreply.github.com>
2025-03-06 15:35:06 +0900

    feat: skillcheck를 위한 gatheringdone 분리

--- src/handlers/gathering/GatheringDone.handler.js
+++ src/handlers/gathering/GatheringDone.handler.js
@@ -5,23 +5,39 @@ import CustomError from '../../utils/error/customError.js';
 import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { addExpHandler } from '../player/addExp.handler.js';
 
-export const gatheringDoneHandler = (socket, packetData) => {
+export const gatheringDoneHandler = async (socket, packetData) => {
   const player = getPlayerSession().getPlayer(socket);
   const sector = getSectorSessions().getSector(player.getSectorId());
   const placedId = player.getGatheringIdx();
+  const player_id = player.getPlayerId();
   if (player.gatheringSuccess) {
     player.gatheringSuccess = false;
 
     const dropItem = sector.resources[placedId].dropItem();
     socket.write(PACKET.S2CGatheringDone(placedId, dropItem, 1));
+    console.log('dropItem : \n', dropItem);
 
-    // 나중에 아이템 받아오는 내역 필요함
-
-    //
+    // 채집한 아이템을 인벤토리 추가하는 로직
+    // dropItem을 Redis 인벤토리에 저장하는 로직 추가
+    try {
+      const slotIdx = await addItemToInventory(socket, player_id, dropItem);
+      console.log(`아이템이 인벤토리 ${slotIdx}번 슬롯에 저장되었습니다.`);
+      // 원한다면 inventoryUpdate.handler.js를 호출해 최신 인벤토리 상태를 클라이언트로 전송할 수 있음
+    } catch (err) {
+      console.error(`인벤토리 업데이트 실패: ${err}`);
+      // 인벤토리 오류 발생 시 클라이언트에 알림 처리 가능
+    }
 
     // addExpHandler(socket, {
     //   count: sector.resources[placedId].getDifficulty(),
     // });
+
+    if (durability <= 0) {
+      setTimeout(
+        () => sector.resetDurability(placedId),
+        sector.resources[placedId].getRespawnTime(),
+      );
+    }
 
     const durability = sector.subDurability(placedId);
 
