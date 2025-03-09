0d6896fecb7fe48ef91ef4a56b043aefe3b6539c
ppiok-OwO <solcorin@naver.com>
2025-03-09 18:51:33 +0900

    fix: 대문자를 바꿔도바꿔도 좀비처럼 되살아나네

--- src/handlers/gathering/gatheringSkillCheck.handler.js
+++ src/handlers/gathering/gatheringSkillCheck.handler.js
@@ -5,6 +5,7 @@ import CustomError from '../../utils/error/customError.js';
 import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { addExpHandler } from '../player/addExp.handler.js';
 import { addItemToInventory } from '../player/inventory/inventoryManager.js';
+import { gatheringDoneHandler } from './gatheringDone.handler.js';
 
 export const gatheringSkillCheckHandler = async (socket, packetData) => {
   const { deltatime } = packetData;
