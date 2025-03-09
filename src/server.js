a288daf3fa8241374658239bc9e367525eb7c4b3
dwook <dwook1637@gmail.com>
2025-03-10 01:04:39 +0900

    feat : elastic agent 접속, test logstash 전송

--- src/server.js
+++ src/server.js
@@ -12,6 +12,31 @@ initServer()
         `[메인서버]가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`,
       );
       console.log(server.address());
+
+      const logstash = new net.Socket();
+      logstash.connect(5000, "59.8.54.12", () => {
+        console.log("Connected to Logstash");
+      });
+
+      setInterval(() => {
+        const log = {
+          timestamp : new Date().toISOString(),
+          level: "info",
+          message: "User logged in",
+          user: "test"
+        };
+
+        console.log(log);
+        logstash.write(JSON.stringify(log) + "\n");
+      }, 5000);
+
+      logstash.on("end", () => {
+        console.error("Logstash Closed:");
+      });
+
+      logstash.on("error", err => {
+        console.error("Logstash Error:", err);
+      });
     });
   })
   .catch((error) => {
