fbfc1d35a2724b411b9084da6f3e277beee75653
songwongbin <bb201802@naver.com>
2025-03-10 16:28:31 +0900

    feat : 보물상자 기능 개선
    1. 최고 등급 아이템 얻도록 수정
    2. 획득한 아이템 채팅으로 알려줌으로써 통일성 유지

--- src/handlers/playerAction/getTreasure.handler.js
+++ src/handlers/playerAction/getTreasure.handler.js
@@ -3,6 +3,9 @@ import { addItemToInventory } from '../player/inventory/inventoryManager.js';
 import PACKET from '../../utils/packet/packet.js';
 
 const REGEN_WAITING = 20000;
+// getGameAssets().item.data를 통해 접근하는 게 옳지만...
+const RARE_ITEM_ID = 20005;
+const RARE_ITEM_NAME = "단열재가 들어간 판자";
 
 const getTreasureHandler = async (socket, packetData) => {
   const player_id = socket.player.playerId; // 레디스 꺼, string
@@ -10,17 +13,15 @@ const getTreasureHandler = async (socket, packetData) => {
   const player = getPlayerSession().getPlayer(socket);
 
   const sector = getSectorSessions().getSector(player.getSectorId());
-  sector.hasChest = false;
+  sector.hasChest = false;  
 
-  const dropItem = sector.resources[1].dropItem(); // 아이템 뭐 줄지 골라야해
-  
   try {
-    const slotIdx = await addItemToInventory(socket, player_id, dropItem);
+    const slotIdx = await addItemToInventory(socket, player_id, RARE_ITEM_ID);
+    socket.write(PACKET.S2CChat(0,`${RARE_ITEM_NAME}아이템을 1개 획득했습니다.`,"System"))
     console.log(`아이템이 인벤토리 ${slotIdx}번 슬롯에 저장되었습니다.`);
-    // 원한다면 inventoryUpdate.handler.js를 호출해 최신 인벤토리 상태를 클라이언트로 전송할 수 있음
   } catch (err) {
+    socket.write(PACKET.S2CChat(0,`아이템 획득에 실패했습니다.`,"System"))
     console.error(`인벤토리 업데이트 실패: ${err}`);
-    // 인벤토리 오류 발생 시 클라이언트에 알림 처리 가능
   }
 
   setTimeout(() => {
