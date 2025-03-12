a6b0d60f58647b8a354e43fea7d13ce4317f7db0
dwook <dwook1637@gmail.com>
2025-03-12 20:42:57 +0900

    Update log.js

--- src/utils/log/log.js
+++ src/utils/log/log.js
@@ -5,55 +5,69 @@ import { setInterval } from 'timers/promises';
 const serverLogs = [];
 const errorLogs = [];
 
-const secPerUpdate = 5;
+const secPerReport = 5;
 
-// 로그 기록
 export function addServerLog(message) {
   const timestamp = Date.now();
   serverLogs.push({ message, time: timestamp });
 }
 
-// 에러 기록
 export function addErrorLog(message) {
   const timestamp = Date.now();
   errorLogs.push({ message, time: timestamp });
 }
 
-export function reportServerLog(){
-    setInterval(() => {
-        if(serverLogs.length<=0) return;
+// 로그 기록
+export function reportServerLog() {
+  console.log('로그 인터벌 시작', serverLogs);
+  setInterval(() => {
+    console.log('로그 기록');
+    if (serverLogs.length <= 0) return;
 
-        for(const log of serverLogs){
-            redisClient.zadd('serverLogs', log.timestamp, JSON.stringify(log),
-            (err) => {
-                if (err) console.error('Redis 서버 로그 저장 실패:', err);
-                else console.log('서버 로그 저장 성공:', message);
-            })
-        }
+    for (const log of serverLogs) {
+      redisClient.zadd(
+        'serverLogs',
+        log.timestamp,
+        JSON.stringify(log),
+        (err) => {
+          if (err) console.error('Redis 서버 로그 저장 실패:', err);
+          else console.log('서버 로그 저장 성공:', message);
+        },
+      );
+    }
 
-        serverLogs = [];
-    }, secPerUpdate * 1000);
+    serverLogs = [];
+  }, secPerReport * 1000);
 }
 
-export function reportErrorLog(){
-    setInterval(() => {
-        if(errorLogs.length <= 0) return;
-    
-        for(const log of errorLogs){
-            redisClient.zadd('errorLogs', log.timestamp, JSON.stringify(log),
-            (err) => {
-                if (err) console.error('Redis 에러 로그 저장 실패:', err);
-                else console.log('에러 로그 저장 성공:', message);
-            })
-        }
-    
-        errorLogs = [];
-    }, secPerUpdate * 1000);
+// 에러 기록
+export function reportErrorLog() {
+  console.log('에러 인터벌 시작');
+  setInterval(() => {
+    console.log('에러 기록');
+    if (errorLogs.length <= 0) return;
+
+    for (const log of errorLogs) {
+      redisClient.zadd(
+        'errorLogs',
+        log.timestamp,
+        JSON.stringify(log),
+        (err) => {
+          if (err) console.error('Redis 에러 로그 저장 실패:', err);
+          else console.log('에러 로그 저장 성공:', message);
+        },
+      );
+    }
+
+    errorLogs = [];
+  }, secPerReport * 1000);
 }
 
 // 메트릭 기록
-export function collectMetric() {
+export function reportMetric() {
+  console.log('메트릭 인터벌 시작');
   setInterval(() => {
+    console.log('메트릭 기록');
     const cpuUsage = os.loadavg()[0];
     const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
     const message = `CPU 사용량: ${cpuUsage}, 메모리 사용량: ${memoryUsage.toFixed(3)} MB`;
@@ -68,5 +82,5 @@ export function collectMetric() {
         else console.log('메트릭 로그 저장 성공:', message);
       },
     );
-  }, secPerUpdate * 1000);
+  }, 3 * 1000);
 }
