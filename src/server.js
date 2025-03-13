abc2cffe6e84e034cac1a6a3678df39bbfabc043
songwongbin <bb201802@naver.com>
2025-03-13 17:06:50 +0900

    feat: 수정 요청 반영

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ import { addServerLog, reportMetric, reportErrorLog, reportServerLog, addErrorLo
 const server = net.createServer(onConnection);
 
 process.on('uncaughtException', (err) => {
-  addErrorLog('예기치 않은 오류 발생:');
+  addErrorLog(`예기치 않은 오류 발생: ${err}`);
 });
-process.on('unhandledRejection', (reason, promise) => {
-  addErrorLog(`Unhandled Rejection:, ${reason}`);
+process.on('unhandledRejection', (reason) => {
+  addErrorLog(`Unhandled Rejection: ${reason}`);
 });
 
 initServer()
