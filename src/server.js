f45f566a257b3d2a9617f706d79bc6c8cd865573
wakelight23 <rlghks0223@gmail.com>
2025-03-13 23:38:11 +0900

    Fix : 코드 수정

--- src/server.js
+++ src/server.js
@@ -2,7 +2,15 @@ import net from 'net';
 import initServer from './init/index.js';
 import { onConnection } from './events/onConnection.js';
 import { config } from './config/config.js';
-import { addServerLog, reportMetric, reportErrorLog, reportServerLog, addErrorLog, reportPacketLog } from './utils/log/log.js';
+import {
+  addServerLog,
+  reportMetric,
+  reportErrorLog,
+  reportServerLog,
+  addErrorLog,
+  reportPacketLog,
+} from './utils/log/log.js';
+import redisClient from './utils/redis/redis.config.js';
 
 const server = net.createServer(onConnection);
 
@@ -12,22 +20,22 @@ process.on('unhandledRejection', (reason, promise) => {
   const stack = reason instanceof Error ? reason.stack : '';
   const match = stack.match(/\((.*):(\d+):(\d+)\)/);
   if (match) {
-      const [_, file, line, column] = match;
-      const message = `Unhandled Rejection: ${reason} (파일: ${file}, 행: ${line}, 열: ${column})`;
-      redisClient.zadd(
-        'errorLogs',
-        timestamp,
-        JSON.stringify({ message, time:timestamp}),
-        (err) => {
-          if (err) console.error('Redis 에러 로그 저장 실패:', err);
-        },
-      );
+    const [_, file, line, column] = match;
+    const message = `Unhandled Rejection: ${reason} (파일: ${file}, 행: ${line}, 열: ${column})`;
+    redisClient.zadd(
+      'errorLogs',
+      timestamp,
+      JSON.stringify({ message, time: timestamp }),
+      (err) => {
+        if (err) console.error('Redis 에러 로그 저장 실패:', err);
+      },
+    );
   } else {
     const message = `Unhandled Rejection: ${reason})`;
     redisClient.zadd(
       'errorLogs',
       timestamp,
-      JSON.stringify({ message, time:timestamp}),
+      JSON.stringify({ message, time: timestamp }),
       (err) => {
         if (err) console.error('Redis 에러 로그 저장 실패:', err);
       },
@@ -38,11 +46,9 @@ process.on('unhandledRejection', (reason, promise) => {
 initServer()
   .then(() => {
     server.listen(config.server.port, config.server.host, () => {
-      console.log(
-        `[메인서버]가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`,
-      );
+      console.log(`[메인서버]가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
       console.log(server.address());
-      
+
       addServerLog(`[메인서버]가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
       reportMetric();
       reportServerLog();
